c9217eab81790aec043d6293b30b6932cb4940d0
Axelgk10 <axgimene@student.42malaga.com>
2025-08-21 21:57:35 +0200

    parsing2.c

---
+++ parsing2.c
@@ -0,0 +1,33 @@
+#include "../push_swap.h"
+
+void	free_split(char **split)
+{
+	int	i;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+		free(split[i++]);
+	free(split);
+}
+
+int	is_valid_digit(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] == ' ' || (s[i] >= '\t' && s[i] <= '\r'))
+		i++;
+	if (s[i] == '-' || s[i] == '+')
+		i++;
+	if (s[i] == '\0')
+		return (0);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
