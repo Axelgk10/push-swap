9173a2e322311b05eef184f6fdc2576432bfe0f5
Axelgk10 <axgimene@student.42malaga.com>
2025-08-25 15:00:34 +0200

    parsing2.c

--- parsing2.c
+++ parsing2.c
@@ -15,19 +15,22 @@ void	free_split(char **split)
 int	is_valid_digit(const char *s)
 {
 	int	i;
+	int	has_digit;
 
 	i = 0;
+	has_digit = 0;
 	while (s[i] == ' ' || (s[i] >= '\t' && s[i] <= '\r'))
 		i++;
 	if (s[i] == '-' || s[i] == '+')
 		i++;
 	if (s[i] == '\0')
 		return (0);
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		has_digit = 1;
 		i++;
 	}
-	return (1);
+	return (has_digit);
 }
